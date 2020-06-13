1b9d07c1aee550b21e3294249c5d90ea3270b196
m-arch <themouallem@gmail.com>
2020-06-13 18:30:05 +0300

    adjusted datatable component to rerender on data update

--- src/re_material_ui_1/custom.cljs
+++ src/re_material_ui_1/custom.cljs
@@ -8,87 +8,91 @@
                                          no-paging? false
                                          identifier :key}}]
   (let [state (r/atom {:order "asc" :page 0 :rows-per-page rows-per-page
-                       :selected []})
-        data-atom (atom data)]
-    (fn []
-      [ui/paper {:style {:width "100%"
-                         :overflow-x "auto"}}
-       [:div {:style {:flex "0 0 auto"}}
-        (when title
-          [ui/typography {:variant "title"
-                          :style (merge {:padding-top "12px"
-                                         :padding-left "22px"}
-                                                  (dissoc head-style :font-size))}
-           title])
-        [:div {:style {:flex "1 1 100%"}}]]
-       [:div {:style {:overflow-x "auto"}}
-        [ui/table {:style {:min-width "700px"}}
-         [ui/table-head
-          [ui/table-row
-           (doall
-            (for [head column-data]
-              (do
-                (when (:eq head)
-                  (reset! data-atom (doall
-                                     (for [item @data-atom]
-                                       (assoc item (:key head) ((:eq head) item))))))
-                [ui/table-cell {:key (:key head)
-                                :numeric (:numeric head)
-                                :style (merge (if (:disable-padding head)
-                                                {:padding "0"}
-                                                {})
-                                              head-style)}
-                 (when (not (:numeric head))
-                   (:label head))
-                 (when (:sortable? head)
-                   [ui/tooltip {:title "sort"
-                                :enter-delay 300}
-                    [ui/table-sort-label {:active (= (:order-by @state) (:key head))
-                                          :direction (:order @state)
-                                          :on-click #(swap! state assoc
-                                                            :order-by (:key head)
-                                                            :order (if (= (:order @state) "asc")
-                                                                                    "desc"
-                                                                                    "asc"))}]])
-                 (when (:numeric head)
-                   (:label head))])))]]
-         [ui/table-body
-          (doall
-           (for [item (if no-paging?
-                        (if (:order-by @state)
-                          (sort-by (:order-by @state)
-                                   (if (= (:order @state) "asc")
-                                     < >) @data-atom)
-                          @data-atom)
-                        (if (:order-by @state)
-                          (take (:rows-per-page @state) (drop (* (:page @state) (:rows-per-page @state))
-                                                              (sort-by (:order-by @state)
-                                                                       (if (= (:order @state) "asc")
-                                                                         < >) @data-atom)))
-                          (take (:rows-per-page @state) (drop (* (:page @state)  (:rows-per-page @state))
-                                                              @data-atom))))]
-             [ui/table-row {:key (identifier item)}
-              (doall
-               (for [[k v] (seq (select-keys item (map #(:key %) column-data)))
-                     :let [head (first (filter #(= (:key %) k) column-data))]]       
-                 [ui/table-cell {:key k
-                                 :style (merge (if (:disable-padding head)
-                                                 {:padding "0"}
-                                                 {})
-                                               cell-style)
-                                 :numeric (:numeric head)
-                                 :component (:component head)}
-                  v]))]))]]]
-       (when (not no-paging?)
-         [ui/table-pagination {:component "div"
-                               :count (count data)
-                               :rows-per-page (:rows-per-page @state)
-                               :page (:page @state)
-                               :back-icon-button-props {:aria-label "Pervious page"}
-                               :next-icon-button-props {:aria-label "Next page"}
-                               :on-change-page #(swap! state assoc :page %2)
-                               :on-change-rows-per-page #(swap! state assoc 
-                                                                :rows-per-page (.. % -target -value))}])])))
+                       :selected []})]
+    (fn [column-data data &{:keys [no-paging? rows-per-page identifier
+                                   head-style cell-style title]
+                            :or {rows-per-page 5
+                                 no-paging? false
+                                 identifier :key}}]
+      (let [data-atom (atom data)]
+        [ui/paper {:style {:width "100%"
+                           :overflow-x "auto"}}
+         [:div {:style {:flex "0 0 auto"}}
+          (when title
+            [ui/typography {:variant "title"
+                            :style (merge {:padding-top "12px"
+                                           :padding-left "22px"}
+                                          (dissoc head-style :font-size))}
+             title])
+          [:div {:style {:flex "1 1 100%"}}]]
+         [:div {:style {:overflow-x "auto"}}
+          [ui/table {:style {:min-width "700px"}}
+           [ui/table-head
+            [ui/table-row
+             (doall
+              (for [head column-data]
+                (do
+                  (when (:eq head)
+                    (reset! data-atom (doall
+                                       (for [item @data-atom]
+                                         (assoc item (:key head) ((:eq head) item))))))
+                  [ui/table-cell {:key (:key head)
+                                  :numeric (:numeric head)
+                                  :style (merge (if (:disable-padding head)
+                                                  {:padding "0"}
+                                                  {})
+                                                head-style)}
+                   (when (not (:numeric head))
+                     (:label head))
+                   (when (:sortable? head)
+                     [ui/tooltip {:title "sort"
+                                  :enter-delay 300}
+                      [ui/table-sort-label {:active (= (:order-by @state) (:key head))
+                                            :direction (:order @state)
+                                            :on-click #(swap! state assoc
+                                                              :order-by (:key head)
+                                                              :order (if (= (:order @state) "asc")
+                                                                       "desc"
+                                                                       "asc"))}]])
+                   (when (:numeric head)
+                     (:label head))])))]]
+           [ui/table-body
+            (doall
+             (for [item (if no-paging?
+                          (if (:order-by @state)
+                            (sort-by (:order-by @state)
+                                     (if (= (:order @state) "asc")
+                                       < >) @data-atom)
+                            @data-atom)
+                          (if (:order-by @state)
+                            (take (:rows-per-page @state) (drop (* (:page @state) (:rows-per-page @state))
+                                                                (sort-by (:order-by @state)
+                                                                         (if (= (:order @state) "asc")
+                                                                           < >) @data-atom)))
+                            (take (:rows-per-page @state) (drop (* (:page @state)  (:rows-per-page @state))
+                                                                @data-atom))))]
+               [ui/table-row {:key (identifier item)}
+                (doall
+                 (for [[k v] (seq (select-keys item (map #(:key %) column-data)))
+                       :let [head (first (filter #(= (:key %) k) column-data))]]       
+                   [ui/table-cell {:key k
+                                   :style (merge (if (:disable-padding head)
+                                                   {:padding "0"}
+                                                   {})
+                                                 cell-style)
+                                   :numeric (:numeric head)
+                                   :component (:component head)}
+                    v]))]))]]]
+         (when (not no-paging?)
+           [ui/table-pagination {:component "div"
+                                 :count (count data)
+                                 :rows-per-page (:rows-per-page @state)
+                                 :page (:page @state)
+                                 :back-icon-button-props {:aria-label "Pervious page"}
+                                 :next-icon-button-props {:aria-label "Next page"}
+                                 :on-change-page #(swap! state assoc :page %2)
+                                 :on-change-rows-per-page #(swap! state assoc 
+                                                                  :rows-per-page (.. % -target -value))}])]))))
 
 (defn input-field [key input-atom & {:keys [class-name style on-change on-blur label
                                             required error-text  type placeholder
